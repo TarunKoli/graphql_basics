ddfe45a1efc5897f3e82aa05f42a090dd55306d2
TarunKoli <amankumar8348@gmail.com>
2022-01-24 16:05:29 +0530

    Wrote my first graphql query

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
-
+const { graphqlHTTP } = require("express-graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
 const app = express();
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "HelloWorld",
+    fields: () => ({
+      message: { type: GraphQLString, resolve: () => "Hello World !" },
+    }),
+  }),
+});
+
+app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true }));
 
 app.listen(6060, () => {
   console.log("Server Listening");
